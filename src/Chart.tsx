285343f7badf9729e1549c178a15c90a3ee4ff84
artiomgreenbuger <iwebschester@gmail.com>
2022-11-03 18:26:13 +0600

    data in chart

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -31,6 +31,32 @@ import Chart from 'react-apexcharts';
 // Название поля 
 // Значение
 
+// Столбцы визуальные столбцы с высотой
+/*** 
+  {
+    name: string, // название графика
+    width: 'full' | 'part', // ширина графика ( на весь экран или на часть экрана)
+    dataLabels: boolean, // отображать значение для бара (визуально)
+    categories: [number, number], // Значения по x (отсортированные)
+    bar_width: number, // ширина всех баров на графике
+    series: [  //  Группа бар для categories отображает массив бар для каждой categories ( для оси Y)
+      { 
+        name:  string // Название бара 1,
+        data: [number, number], // массив  всех бар1 на графике с индексом categories ( categories.length == data.lenght)
+        color: string, // цвет  в формате HEX для бара
+      },
+      { 
+        name: string // Название бара 2,
+        data: [number, number],// массив всех бар2 на графике с индексом categories ( categories.length == data.lenght)
+        color: string,
+      },
+      ...
+    ]
+  }
+]
+***/
+
+
 const ChartBar = () => {
   
   const chartData: ApexOptions = {
@@ -60,22 +86,22 @@ const ChartBar = () => {
       xaxis: {
         categories: [1991, 1992, 1993, 1994, 1995, 1996]
       },
-      fill:{
-        colors: ['#F44336', '#E91E63', '#9C27B0'],
-      },
-      // fill: {
-      //   type: 'gradient',
-      //   gradient: {
-      //     shade: 'light',
-      //     type: "horizontal",
-      //     shadeIntensity: 0.25,
-      //     gradientToColors: undefined,
-      //     inverseColors: true,
-      //     opacityFrom: 0.85,
-      //     opacityTo: 0.85,
-      //     stops: [50, 0, 100]
-      //   },
+      // fill:{
+      //   colors: ['#F44336', '#E91E63', '#9C27B0'],
       // },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          type: "horizontal",
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 0.85,
+          opacityTo: 0.85,
+          stops: [50, 0, 100]
+        },
+      },
   };
   return (
     <div className='chart_wrapper'>
